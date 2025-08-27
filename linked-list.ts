ed8aa0900b9ad328f15a58cd9c82326b562fece0
subat <subat.yucel1@gmail.com>
2025-08-27 15:06:57 +0300

    insert & remove at a specific position methods implemented to LinkedList class

--- linked-list.ts
+++ linked-list.ts
@@ -23,6 +23,53 @@ class LinkedList<T> {
     this.length++;
   }
 
+  insertAt(value: T, pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root;
+      let index = 0;
+      let previous = current;
+      let node = new ListNode(value);
+
+      if (pos === 0) {
+        node.next = this.root;
+        this.root = node;
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;
+        previous.next = node;
+      }
+      this.length++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  removeAt(pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root;
+      let previous = current;
+      let index = 0;
+
+      if (pos === 0) {
+        this.root = current.next;
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.length--;
+      return current.value;
+    } else {
+      return null;
+    }
+  }
+
   getNumberOfElements() {
     return this.length;
   }
@@ -45,7 +92,18 @@ numberList.add(14);
 numberList.add(16);
 numberList.add(18);
 
-console.log(numberList.getNumberOfElements());
+console.log("Element Count: " + numberList.getNumberOfElements());
+console.log("Elements : ");
 numberList.print();
 
-const nameList = new LinkedList<string>();
+console.log(numberList.insertAt(5, 3));
+console.log(
+  "Element Count Afer insertion: " + numberList.getNumberOfElements()
+);
+console.log("Elements : ");
+numberList.print();
+
+console.log(numberList.removeAt(2));
+console.log("Element Count Afer deletion: " + numberList.getNumberOfElements());
+console.log("Elements : ");
+numberList.print();
